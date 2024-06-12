a93b9bcdfa20913c8bf8bf898bbd830ea0fbe333
DiogenesDv <diogenesdesenvolvedor@gmail.com>
2024-06-11 23:34:51 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keywords" content="Delivery COLOCAR NOME AQUI" />
+    <meta name="description" content="DESCRIÇÃO DOS SERVIÇOS DO DELIVERY ..." />
+    <meta name="author" content="NOME DO DONO ..." />
+    <!-- icone -->
+    <link rel="shortcut icon" href="img/pizza-svgrepo-com.svg" type="image/x-icon">
+    <!-- Bootstrap com js para estilização,icones,animaçoes e css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Delivery Interativo</title>
+</head>
+
+<body>
+    <main class="main_container">
+        <header>
+            <nav class="navbar bg-body-tertiary fixed-top header_top">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="img/pizza-svgrepo-com.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                        Delivery Interativo
+                    </a>
+                    <big>
+                        <span class="bi bi-cart4 icone_cart">
+                            <small><small>
+                                    <span class="position-absolute top-10 start-90 translate-middle badge rounded-pill bg-danger">
+                                        0
+                                        <span class="visually-hidden">unread messages</span>
+                                    </span>
+                            </small></small>
+                    </big>
+                </div>
+            </nav>
+        </header>
+
+        <section class="row cards_">
+            <div class="col-md-4 col-5 card_">
+                <h3>
+                    Pizza
+                </h3>
+            </div>
+            <div class="col-md-4 col-5 card_">
+                <h3>
+                    Pizza
+                </h3>
+            </div>
+            <div class="col-md-4 col-5 card_">
+                <h3>
+                    Pizza
+                </h3>
+            </div>
+        </section>
+        
+    </main>
+</body>
+
+</html>
