21e672c8fb7a06dacd5c1bff68880a90309be2f3
DiogenesDv <diogenesdesenvolvedor@gmail.com>
2024-08-28 22:11:21 -0300

    al 45 ok

--- observacoes.php
+++ observacoes.php
@@ -2,9 +2,9 @@
 
 <nav class="navbar bg-body-tertiary fixed-top header_top">
   <div class="container-fluid">
-    <span>Resumo do Pedido</span>
+    <span>Resumo do Item</span>
     <div class=".icone_seta_voltar_">
-      <a class="navbar-brand" href="index.php" alt="Voltar" title="Botão Voltar">
+      <a class="navbar-brand" href="adicionais.php" alt="Voltar" title="Botão Voltar">
         <big><i class="bi bi-arrow-left-circle icone_seta_voltar_"></i></big>
       </a>
     </div>
@@ -26,6 +26,38 @@
 
 </div>
 
+<div class="destaque_qtd_2">
+  <strong>Observações</strong>
+  <div class="form-group">
+    <textarea class="form-control" type="text" name="obs"></textarea>
+  </div>
+</div>
+
+<div class="total_">
+  <b>Total R$ 0,0</b>
+</div>
+
+<div class="botao_ d-grid">
+ <a href="#popup2" class="btn btn-primary">Adicionar ao carrinho  <i class="bi bi-cart4"></i></a>
+</div>
+
 </body>
 
 </html>
+
+<div id="popup2" class="overlay_2">
+    <div class="popup_2">
+        <div class="row">
+            <div class="col-9">
+                <h3 class="titulo-popup">3 Itens Adicionados</h3>
+            </div>
+            <div class="col-3">
+                <a class="close" href="#">&times;</a>
+            </div>
+        </div>
+        <hr class="linha">
+        <div class="conteudo-popup">
+            Aqui vamos colocar depois o conteúdo desse popup trazendo os itens que forem adicionados no carrinho
+        </div>
+    </div>
+</div>
