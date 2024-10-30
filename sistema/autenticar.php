f60a6d5b58e1d66c858f37d7aa6c48ea5fd3cab6
DiogenesDv <diogenesdesenvolvedor@gmail.com>
2024-10-30 20:04:31 -0300

    aul 27 ok

--- sistema/autenticar.php
+++ sistema/autenticar.php
@@ -1,21 +1,34 @@
 <?php
-
+@session_start();//Para criar variaveis de sessao(variaveis que poderao guardar imformacoes de pagina anteior para serem ultilizadas na proxima)
 require_once('conexao.php');
 
 //Recuperando valores
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-echo $email;
+//echo $email;
 
 //VERIFICAR(pessquisar) SE EXISTE USUÁRIO ADMINISTRADOR CA-DASTRADO
- $query = $pdo->query("SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha' ");
+ $query = $pdo->query("SELECT * FROM usuarios WHERE (email = '$email' or cpf = '$email') and senha = '$senha' ");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $total_reg = @count($res);
 
 if($total_reg > 0){
-    echo ' Você esta logado!';
+    $_SESSION['id'] = $res[0]['id'];
+    $_SESSION['nome'] = $res[0]['nome'];
+    $_SESSION['nivel'] = $res[0]['nivel'];
+    $_SESSION['ativo'] = $res[0]['ativo'];
+
+    if ($_SESSION['ativo']
+     == 'sim') {
+        echo "<script>window.location='painel'</script>";
+    }else{
+        echo "<script>window.alert('USUÁRIO INATIVO!!!')</script>";
+        echo "<script>window.location='index.php'</script>";    
+    }
+    
 }else{
-    echo ' Não Logado';
+    echo "<script>window.alert('USUÁRIO ou SENHA Incorretos!!!')</script>";
+    echo "<script>window.location='index.php'</script>";
 }
 ?>
